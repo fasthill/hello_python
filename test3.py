7c66c2ad0e7ec4c2ad1bb0bf34c77873a6e8a339
fasthill <kangeun@naver.com>
2021-03-14 19:00:24 +0900

    home delete1

--- test3.py
+++ test3.py
@@ -3,8 +3,9 @@ modified local collaborator
 def test(a):
     return a**2
 
+list2 = []
 list1 = [1,2,3,4,5,6,7]
 list3 = []
 
-for i in list1:
+for i in list2:
     print("i : {},  a = {}".format(i, test(i)))
