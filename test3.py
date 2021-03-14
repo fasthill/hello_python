a1a779beb15a18e7634c4f2cdaa20ec83e9589ac
fasthill <kangeun@naver.com>
2021-03-14 19:23:51 +0900

    add test3

---
+++ test3.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import csv
+
+def test(a):
+    return a**2
+
+list2 = []
+list1 = [1,2,3,4,5,6,7]
+
+for i in list2:
+    print("i : {},  a = {}".format(i, test(i)))
